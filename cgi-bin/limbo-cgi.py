11979cecdd6ad8b0976534ed9f3598eb301c5e1d
xander <xander@orion.caltech.edu>
2014-06-13 19:38:52 -0700

    Made it so there is only one cgi request on loading store.html

--- cgi-bin/limbo-cgi.py
+++ cgi-bin/limbo-cgi.py
@@ -15,8 +15,8 @@ form = cgi.FieldStorage()
 # There's a decent sqlite tutorial here: http://zetcode.com/db/sqlitepythontutorial/
 database = 'limbo4.db'
 
-# How to use decimal example:
-# Decimal('14.524').quantize(Decimal('.01'), rounding=ROUND_DOWN)
+def round(decimal):
+    decimal.quantize(Decimal('.01'), rounding=ROUND_DOWN)
 
 def initialize_test_database():
     """This function should only be called on an empty new database. This
@@ -33,26 +33,26 @@ def initialize_test_database():
         # Stock is no longer valid if Count is zero or if it's after the expiry
         # date.
         connection.execute("""CREATE TABLE Items(
-                            Name         TEXT NOT NULL UNIQUE,
-                            Seller       TEXT NOT NULL,
-                            Count     INTEGER NOT NULL,
-                            Price        TEXT NOT NULL,
-                            StockDate    TEXT NOT NULL,
-                            ExpiryDate   TEXT NOT NULL,
-                            Description  TEXT)""")
+                              Name         TEXT NOT NULL UNIQUE,
+                              Seller       TEXT NOT NULL,
+                              Count     INTEGER NOT NULL,
+                              Price        TEXT NOT NULL,
+                              StockDate    TEXT NOT NULL,
+                              ExpiryDate   TEXT NOT NULL,
+                              Description  TEXT)""")
         # A "Transaction" is either a purchase, money transfer, withdrawal, or
         # deposit. For a purchase, all fields need to be filled. For a transfer,
         # the "item" field is NULL. For a withdrawal, the Seller and Item fields
         # are NULL. For a deposit, the Buyer and Item fields are NULL.
         connection.execute("""CREATE TABLE Transactions(
-                            Date       TEXT NOT NULL,
-                            Buyer      TEXT,
-                            Seller     TEXT,
-                            Item       INTEGER,
-                            Count      INTEGER NOT NULL,
-                            Price      TEXT NOT NULL,
-                            StockDate  TEXT NOT NULL,
-                            ExpiryDate TEXT NOT NULL)""")
+                              Date       TEXT NOT NULL,
+                              Buyer      TEXT,
+                              Seller     TEXT,
+                              Item       INTEGER,
+                              Count      INTEGER NOT NULL,
+                              Price      TEXT NOT NULL,
+                              StockDate  TEXT NOT NULL,
+                              ExpiryDate TEXT NOT NULL)""")
         # Now we will populate the Users and Items tables with some fake users
         # and items.
         add_user("jrmole", "jrmole@blacker.caltech.edu")
@@ -61,7 +61,7 @@ def initialize_test_database():
         add_user("srmole", "srmole@blacker.caltech.edu")
         
         add_item("snapple", "jrmole", 24, Decimal('1.23'), 52, 'A Juicy Beverage')
-        add_item("apple snapple", "mole", 24, Decimal('1.55'), 24, 'A Juicy Beverage in a glass container')
+        add_item("Lorem ipsum", "mole", 2, Decimal('1.55'), 24, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
     return True
 
 def delete_test_database():
@@ -84,12 +84,12 @@ def add_user(username, email):
         connection.execute("INSERT INTO Users VALUES(?, ?, ?, ?)",
                            (username, email, str(balance), joindate))
 
-def get_inventory():
+def get_all_stock():
     with sql.connect(database) as connection:
-        rows = list(connection.execute("SELECT * FROM Items ORDER BY Name"))
+        rows = list(connection.execute("SELECT * FROM Items"))
     return rows
 
-def get_stock(username):
+def get_user_stock(username):
     rows = []
     with sql.connect(database) as connection:
         for row in connection.execute("SELECT * FROM Items WHERE Name=?", (username,)):
@@ -111,11 +111,22 @@ def get_username(username):
     else:
         return False
 
+def get_store_info(username):
+    """This request returns all the information needed when store.html loads.
+       This includes the username, all in-stock items, all items being sold by
+       the user, and past transactions of that user."""
+    userinfo = get_username(username)
+    if not username:
+        return False
+    my_stock = get_user_stock(username)
+    all_stock = get_all_stock()
+    return (userinfo, my_stock, all_stock)
+
 # These are the allowed actions of cgi requests.
 actions = {
     "usernames": get_usernames,
-    "inventory": get_inventory,
     "username": get_username,
+    "store_info": get_store_info,
     "init_test_database": initialize_test_database, # remove this line in production
     "delete_test_database": delete_test_database, # remove this line in production
 }
